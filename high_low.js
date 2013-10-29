2d0ba98adacbfe2f98242c9c185da705d472b224
Amal Hussein <amalhussein08@gmail.com>
2013-10-28 22:18:48 -0400

    finished first part, and working on second problem now

--- high_low.js
+++ high_low.js
@@ -1,1 +1,33 @@
  
+// Also, write a simple 'game' that makes a user guess a number between 1 and 10.
+// If they are incorrect, output if they are high or low. If they are correct, output 
+// that they are correct and end the game. 
+// Put these in their respective javascript files. 
+
+var 
+guess = prompt("Hello Ninja, guess what number I'm thinking?"); 
+answer = Math.random() * (1 - 10)
+
+while 
+	(guess !== answer) {
+	console.log("Wrong guess, try again!");
+	guess = prompt("Guess again!"); 
+	}
+else {
+	console.log("")
+}
+
+
+
+
+
+	x = 1;
+while (x < 11){
+	if (x%2 ===0){
+		console.log("X is even");
+	} 
+	else {
+		console.log("X is odd");
+	}
+	x = x + 1;
+}
--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,31 @@
- 
+// Write a simple interest calculator for loans (except Bryce, who will write compound). 
+// Ask the user for the total amount borrowed, interest rate and number of years.
+
+
+var loan, years, interest; 
+loan = prompt("What the loan amount?");
+years = prompt("Whats the numbers of years for the loan payments?");
+interest = prompt("Whats the interest rate of the loan?");
+
+var total_interest = loan*years*interest; 
+
+var total = parseFloat(total_interest) + parseFloat(loan);
+
+console.log("$" + total_interest + " is the total amount of interest paid on your loan!");
+console.log("$"+ total + " is the full repayment of the loan. ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
